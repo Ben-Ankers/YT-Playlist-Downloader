d7d2dfa5b05b82d204a7fd4e55aec5c5e1a3051a
Ben <ben_ankers@yahoo.co.uk>
2022-05-26 17:30:00 +0100

    Made much faster
    Moved things around and made a lot faster

--- Main.py
+++ Main.py
@@ -3,27 +3,31 @@ from pytube import Playlist
 
 musicList = ("C:\\Users\\Ben\\Desktop\\musicFolderTest\\MusicList.txt")
 musicFile = open(musicList,'a')
-ytPlaylist = Playlist('https://www.youtube.com/playlist?list=PLiB4gGXb4mNJOgroZ3ZjYI_PJvnUvk3y4')
+
+ytPlaylist = Playlist('https://www.youtube.com/playlist?list=PLiB4gGXb4mNJtS2gCVaAemHKVFHZmJDyS')
+
 
 for url in ytPlaylist.video_urls:
     yt = YouTube(url)
     #dl = yt.streams.get_highest_resolution()
-    dl = yt.streams.filter(only_audio = True)
-    print(yt.title)
-
     hasFile = False
 
     with open(musicList, 'r') as file:
         for line in file:
             if url+"\n" == line:
                 hasFile = True
+                print("File already downloaded")
 
 
     if hasFile == False:
+        print("Getting Video")
+        dl = yt.streams.filter(only_audio = True)
+        print("Video Got!")
         dl[0].download("C:\\Users\\Ben\\Desktop\\musicFolderTest\\Musics")
         musicFile.write(url+'\n')
+        print("Downloading " + yt.title)
+
 
 
 musicFile.close()
-
-
+print("Download has been completed!")
